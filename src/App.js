71f1000827ff47114e00d325b0587b19badbbc7c
Adeshina Ibrahim <debowale01@gmail.com>
2020-09-10 14:18:10 +0100

    App component cleanup

--- src/App.js
+++ src/App.js
@@ -8,19 +8,6 @@ import { Provider } from "./context";
 import "./App.css";
 
 class App extends Component {
-  //handle add item
-  // handleClick = (description, amount) => {
-  //   const id = Math.floor(Math.random() * 1000000);
-  //   const type = +amount > 0 ? "inc" : "exp";
-  //   const newItem = {
-  //     id,
-  //     description,
-  //     amount: +amount,
-  //     type,
-  //   };
-
-  //   this.setState({ allItems: [...this.state.allItems, newItem] });
-  // };
   render() {
     return (
       <Provider>
